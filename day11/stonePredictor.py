5bd4300ec9c3f5037dae76ea103090413eab1025
shephardluke <shephardluke@hotmail.com>
2024-12-24 22:57:26 +0000

    Add day 11 part 2 solution

--- day11/stonePredictor.py
+++ day11/stonePredictor.py
@@ -1,41 +1,69 @@
 # Main method
 def stonePredictor():
-    BLINKS = 25
+    BLINKS = 75
 
     FILE_NAME = "input.txt"
 
-    stones = getStonesFromFile(FILE_NAME)
+    amounts = toHashmap(getStonesFromFile(FILE_NAME))
 
-    print(len(predictStonesMultiple(stones, BLINKS)))
+    newAmounts = predictStonesMultiple(amounts, BLINKS)
+
+    print(getAmounts(newAmounts))
 
 
 # Returns new stones after x times blinked
-def predictStonesMultiple(stones, times):
-    newStones = stones
-
-    for _ in range(times):
-        newStones = predictStones(newStones)
-    
-    return newStones
+def predictStonesMultiple(amounts, times):
+    for i in range(times):
+        amounts = predictStones(amounts)
+    return amounts
 
 
 # Returns new stones after 1 blink
-def predictStones(stones):
-    newStones = []
+def predictStones(amounts):
 
-    for stone in stones:
+    newAmounts = {}
+
+    for stone in list(amounts):
         if stone == 0:
-            newStones.append(1)
+            addToAmounts(newAmounts, 1, amounts[stone])
         elif len(str(stone)) % 2 == 0:
             stoneString = str(stone)
             stoneMiddle = int(len(stoneString) / 2)
 
-            newStones.append(int(stoneString[:stoneMiddle:]))
-            newStones.append(int(stoneString[stoneMiddle::])) 
+            addToAmounts(newAmounts, int(stoneString[:stoneMiddle:]), amounts[stone])
+            addToAmounts(newAmounts, int(stoneString[stoneMiddle::]), amounts[stone])
         else:
-            newStones.append(stone * 2024)
+            addToAmounts(newAmounts, stone*2024, amounts[stone])
 
-    return newStones
+    return newAmounts
+
+
+# Returns the total of all the amounts
+def getAmounts(amounts):
+    total = 0
+    for item in list(amounts):
+        total += amounts[item]
+    return total
+
+
+# Adds a value to the amounts hashmap
+def addToAmounts(amounts, item, amount):
+    if item in amounts:
+        amounts[item] += amount
+    else:
+        amounts[item] = amount
+
+
+def toHashmap(array):
+    items = {}
+
+    for item in array:
+        if item in items:
+            items[item] += 1
+        else:
+            items[item] = 1
+    
+    return items
 
 
 # Reads stones from file and returns them as an array of ints
