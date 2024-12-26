b418dfb55a50ffc1dd8b187564778ee1ce6f09d1
shephardluke <shephardluke@hotmail.com>
2024-12-26 15:06:54 +0000

    Add day 12 part 2 solution

--- day12/fencePriceCalculator.py
+++ day12/fencePriceCalculator.py
@@ -1,68 +1,142 @@
 # Main method
 def fencePriceCalculator():
+    BULK_DISCOUNT_ENABLED = False
+
     FILE_NAME = "input.txt"
 
     garden = readFromFile(FILE_NAME)
 
-    print(getFencePrice(garden))
+    print(getFencePrice(garden, BULK_DISCOUNT_ENABLED))
+
+
+# Returns the number of sides from an array of points that share one axis
+def getSides(array, sharedAxis):
+    sameAxis = getSameAxis(array, sharedAxis)
+
+    sides = 0
+    
+    for i in list(sameAxis.keys()):
+        array = sameAxis[i]
+        array.sort()
+
+        sides += getGroups(array)
+
+    return sides
+
+
+# Returns a hashmap with all the shared axis of each point being the key and the other value being the value, so [(1, 2), (3, 2), (5, 6)], 1 would return {2: [1, 3], 6: [5]}
+def getSameAxis(array, sharedAxis):
+    sameAxis = {}
+    
+    for item in array:
+        value = item[sharedAxis]
+        otherAxis = (sharedAxis + 1) % 2
+        if value in sameAxis:
+            sameAxis[value].append(item[otherAxis])
+        else:
+            sameAxis[value] = [item[otherAxis]]
+
+    return sameAxis
+
+
+# Takes a sorted array and returns how many sections of consecutive numbers there are, for example [1, 2, 3, 5, 6] would return 2
+def getGroups(sortedArray):
+    pointer = sortedArray[0]
+    groups = 1
+
+    for item in sortedArray:
+        if item == pointer:
+            pointer += 1
+        else:
+            pointer = item
+            pointer += 1
+            groups += 1
+    
+    return groups
 
 
 # Returns the price of all the fences for the garden 
-def getFencePrice(garden):
+# When bulk discount is enabled all the sides get calculated otherwise its just the count of the edges
+def getFencePrice(garden, bulkDiscountEnabled):
     price = 0
 
     for y in range(len(garden)):
         for x in range(len(garden[y])):
             if not type(garden[y][x]) is int:
                 details = getPlotDetails(garden, x, y, garden[y][x])
-                price += details[0] * details[1]
+
+                if bulkDiscountEnabled:
+                    sides = 0
+                    for i in range(len(details[1])):
+                        sides += getSides(details[1][i], (i + 1) % 2)
+                    price += details[0] * sides
+                else:
+                    price += details[0] * lenNestedArray(details[1])
 
     return price
 
 
-# Adds up all the area and perimeters of the plots recursively and returns it as (area, perimeter)
+# Adds up all the area and get the edges of the plots recursively and returns it as (area, edges)
+# Edges is an array [[], [], [], []] filled with which plots have an edge in that direction
+# Direction is always top, left, bottom, right (clockwise)
 def getPlotDetails(garden, x, y, label):
-    if garden[y][x] != label:
-        return (0, 0)
-
-    total = (0, 0)
+    if not inBounds(garden, x, y) or garden[y][x] != label:
+        return (0,  [[], [], [], []])
 
     toAdd = [
-        (x - 1, y),
         (x, y - 1),
         (x + 1, y),
-        (x, y + 1)
+        (x, y + 1),
+        (x - 1, y)
     ]
 
-    garden[y][x] = ord(label)
+    garden[y][x] = ord(garden[y][x])
 
-    for pos in toAdd:
-        result = checkSurroundingPlots(garden, pos, label)
-        total = (total[0] + result[0], total[1] + result[1])
+    totalArea, totalEdges = getTotals(garden, x, y, label, toAdd)
 
-    return (total[0] + 1, total[1])
+    return (totalArea + 1, totalEdges)
 
 
-# Checks the surrounding plots, if they match then their plots are checked, but if they dont match the perimeter goes up. Returns in the form (area, perimeter)
-def checkSurroundingPlots(garden, pos, label):
+# Returns totals of area and edges for the new edges that get checked
+def getTotals(garden, x, y, label, toAdd):
 
-    if inBounds(garden, pos[0], pos[1]):
-        if (garden[pos[1]][pos[0]] == ord(label)):
-            return (0, 0)
+    edges = getEdges(garden, label, toAdd)
 
-        result = getPlotDetails(garden, pos[0], pos[1], label)
+    totalEdges = [[], [], [], []]
+    totalArea = 0
 
-        if result == (0, 0):
-            return (0, 1)
-        
-        return (result[0], result[1])
+    for i in range(len(edges)):
+        if edges[i]:
+            totalEdges[i].append((x, y))
+        else:
+            next = getPlotDetails(garden, toAdd[i][0], toAdd[i][1], label)
+            totalArea += next[0]
+            for j in range(len(next[1])):
+                totalEdges[j] += next[1][j]
     
-    return (0, 1)
+    return (totalArea, totalEdges)
 
 
-# Returns true on if x and y are inside the nested array
-def inBounds(nestedArray, x, y):
-    return x >= 0 and y >= 0 and y <= len(nestedArray) - 1 and x <= len(nestedArray[y]) - 1
+# Returns a boolean array of which side has edges
+def getEdges(garden, label, toAdd):
+    edges = []
+
+    for i in range(len(toAdd)):
+        edges.append(isEdge(garden, toAdd[i][0], toAdd[i][1], label))
+    
+    return edges
+
+
+# Returns False only when this plot has the same label as the plot thats checking it
+def isEdge(garden, toCheckX, toCheckY, label):
+
+    if inBounds(garden, toCheckX, toCheckY):
+        if garden[toCheckY][toCheckX] != label and garden[toCheckY][toCheckX] != ord(label):
+            return True
+
+        return False
+    
+    return True
 
 
 # Reads a garden from the given file and return it as a nested array
@@ -74,6 +148,19 @@ def readFromFile(fileName):
         garden.append(list(line.rstrip()))
     
     return garden
+
+
+# Returns the total length of all the subarrays
+def lenNestedArray(array):
+    totalLen = 0
+    for y in range(len(array)):
+        totalLen += len(array[y])
+    return totalLen
+
+
+# Returns true on if x and y are inside the nested array
+def inBounds(nestedArray, x, y):
+    return x >= 0 and y >= 0 and y <= len(nestedArray) - 1 and x <= len(nestedArray[y]) - 1
 
 
 fencePriceCalculator()
